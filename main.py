96d985062309c7f3d0913f03a73f6220c2a5b874
shipun45 <hellomayya1@gmail.com>
2023-04-05 20:27:40 +0530

    crafting

--- main.py
+++ main.py
@@ -2,3 +2,22 @@ from instagrapi import Client
 
 username = input("Username: ") 
 password = input("Password: ")
+client = Client()
+client.login(username,password)
+
+
+hastag = input("Hastag: ")
+amount = int(input("Amount: "))
+posts = client.hashtag_medias_recent(hastag,amount)
+
+for i in range(amount):
+    print("Post " + str(i +1))
+    client.media_like(posts[i].id)
+    print("Liked post")
+    client.user_follow(posts[i].user.pk)
+    print("Followed " + posts[i].user.username)
+    
+    
+    
+    
+    
